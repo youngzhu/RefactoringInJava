0bda85019af470285166b767516ecd01c3e82aa8
young-win <young@win.com>
2020-03-31 13:46:16 +0800

    第一版，基础类+测试类，完成

---
+++ src/main/java/com/youngzy/refactoring/edition1/ch01/Customer.java
@@ -0,0 +1,87 @@
+package com.youngzy.refactoring.edition1.ch01;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String name;
+    private Vector rentals = new Vector();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.addElement(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+
+        Enumeration ss = rentals.elements();
+
+        String result = "Rental Record for " + this.name + "\n";
+
+        while (ss.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) ss.nextElement();
+
+            // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints ++;
+
+            // add bonus for a two day new release rental
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                    && each.getDaysRented() > 1) {
+                frequentRenterPoints ++;
+            }
+
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle()
+                        + "\t" + thisAmount + "\n";
+
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vector getRentals() {
+        return rentals;
+    }
+
+    public void setRentals(Vector rentals) {
+        this.rentals = rentals;
+    }
+}
---
+++ src/test/java/com/youngzy/refactoring/edition1/ch01/CustomerTest.java
@@ -0,0 +1,33 @@
+package com.youngzy.refactoring.edition1.ch01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @Test
+    public void statement() {
+        Movie movie1 = new Movie("英雄本色", Movie.NEW_RELEASE);
+        Movie movie2 = new Movie("佩奇", Movie.CHILDREN);
+
+        Rental rental1 = new Rental(movie1, 5);
+        Rental rental2 = new Rental(movie2, 2);
+
+        Customer customer = new Customer("张三");
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+
+        String statement = customer.statement();
+        System.out.println(statement);
+
+        String expected = "Rental Record for 张三\n" +
+                "\t英雄本色\t15.0\n" +
+                "\t佩奇\t1.5\n" +
+                "Amount owed is 16.5\n" +
+                "You earned 3 frequent renter points";
+
+        Assert.assertEquals(expected, statement);
+    }
+}
